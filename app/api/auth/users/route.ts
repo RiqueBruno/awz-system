d0bc8c45a15e9db886b54df61dc6d0d0a3326cd7
Bruno H Cardoso <riquebruno@yahoo.com.br>
2025-04-28 18:02:43 -0300

    Refactor user data retrieval in GET function and improve formatting

--- app/api/auth/users/route.ts
+++ app/api/auth/users/route.ts
@@ -1,33 +1,38 @@
-import {GoogleSpreadsheet} from "google-spreadsheet"; 
-import {JWT} from "google-auth-library";
+import { GoogleSpreadsheet } from "google-spreadsheet";
+import { JWT } from "google-auth-library";
+import { UserLogin } from "@/app/types/userLogin";
 
-export const dynamic = 'force-static'; // This will make sure the page is not cached by the browser
+export const dynamic = "force-static"; // This will make sure the page is not cached by the browser
 
 const serviceAccountAuth = new JWT({
-    email: process.env.GOOGLE_CLIENT_EMAIL,
-    key: process.env.GOOGLE_PRIVATE_KEY,
-    scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
-  });
+  email: process.env.GOOGLE_CLIENT_EMAIL,
+  key: process.env.GOOGLE_PRIVATE_KEY,
+  scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+});
 
-  export async function GET() {
-    const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID as string, serviceAccountAuth);
-    await doc.loadInfo();
+export async function GET() {
+  const doc = new GoogleSpreadsheet(
+    process.env.GOOGLE_SHEET_ID as string,
+    serviceAccountAuth
+  );
+  await doc.loadInfo();
 
-    try {
-        const sheet = doc.sheetsByTitle['users'];
-        const rows = await sheet.getRows();
-        const users = rows.map((row) => {
-            return {
-                id: row.id,
-                nickname: row.nickname,
-                name: row.name,
-                email: row.email,
-                role: row.role,
-            };
-        });
-        return Response.json(users);
-    } catch (error) {
-        console.error("Error fetching users:", error);
-        return Response.json({ error: "Failed to fetch users" }, { status: 500 });
-    }
+  try {
+    const sheet = doc.sheetsByTitle["users"];
+    const rows = await sheet.getRows();
+    const users: UserLogin[] = rows.map((row) => {
+      return {
+        id: row.get("id") || "",
+        nickname: row.get("nickname") || "",
+        name: row.get("name") || "",
+        createdAt: row.get("createdAt") || "",
+        lastLoginAt: row.get("lastLoginAt") || "",
+        loginHistory: row.get("loginHistory") || [],
+      };
+    });
+    return Response.json(users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    return Response.json({ error: "Failed to fetch users" }, { status: 500 });
   }
+}
