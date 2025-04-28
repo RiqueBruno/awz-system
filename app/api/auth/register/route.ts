1125c248354bef9212c77788a083d64495302816
Bruno H Cardoso <riquebruno@yahoo.com.br>
2025-04-28 18:02:50 -0300

    Refactor user registration logic to improve error handling and add user creation functionality

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,24 +1,90 @@
-import {GoogleSpreadsheet} from "google-spreadsheet"; 
-import {JWT} from "google-auth-library";
+import { GoogleSpreadsheet } from "google-spreadsheet";
+import { JWT } from "google-auth-library";
+import { NewUserRegister, UserRegister } from "@/app/types/userRegister";
 
-export const dynamic = 'force-static'; // This will make sure the page is not cached by the browser
+export const dynamic = "force-static";
 
 const serviceAccountAuth = new JWT({
-    email: process.env.GOOGLE_CLIENT_EMAIL,
-    key: process.env.GOOGLE_PRIVATE_KEY,
-    scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
-  });
+  email: process.env.GOOGLE_CLIENT_EMAIL,
+  key: process.env.GOOGLE_PRIVATE_KEY,
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+});
 
-  const serviceAccountAuthEdit = new JWT({
-    email: process.env.GOOGLE_CLIENT_EMAIL,
-    key: process.env.GOOGLE_PRIVATE_KEY,
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-  });
+export async function POST(request: Request) {
+  const doc = new GoogleSpreadsheet(
+    process.env.GOOGLE_SHEET_ID as string,
+    serviceAccountAuth
+  );
+  await doc.loadInfo();
 
-  export async function GET() {
-    const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID as string, serviceAccountAuth);
-    await doc.loadInfo();
+  try {
+    const sheet = doc.sheetsByTitle["users"];
+    const user: Promise<UserRegister> = request.json();
 
-    const sheet = doc.sheetsByTitle['users'];
+    if (!user) {
+      return new Response(JSON.stringify({ error: "Invalid data" }), {
+        status: 404,
+      });
+    }
+
+    const { nickname, name, passwordHash } = await user;
+    if (!nickname || !name || !passwordHash) {
+      return new Response(
+        JSON.stringify({ error: "Missing required fields" }),
+        { status: 404 }
+      );
+    }
+
     const rows = await sheet.getRows();
+    const existingUser = rows.map((row) => ({
+      id: row.get("id") || "",
+      nickname: row.get("nickname") || "",
+    }));
+
+    const userEmailExists = existingUser.some(
+      (row) => row.nickname === nickname
+    );
+
+    const id =
+      existingUser.reduce((maxId, row) => {
+        const rowId = Number(row.id);
+        return Math.max(maxId, rowId);
+      }, 0) + 1;
+
+    if (userEmailExists) {
+      return new Response(
+        JSON.stringify({ error: "User with this nickname already exists" }),
+        { status: 409 }
+      );
+    }
+
+    const date: string = new Date()
+      .toLocaleDateString("pt-BR", { timeZone: "America/Sao_Paulo" })
+      .toString();
+    const time: string = new Date()
+      .toLocaleTimeString("pt-BR", { timeZone: "America/Sao_Paulo" })
+      .toString();
+    const dateTime: string = `${date},${time}`;
+
+    const newUser: NewUserRegister = {
+      id: id.toString(),
+      nickname,
+      name,
+      passwordHash,
+      createdAt: dateTime,
+      lastLoginAt: dateTime,
+      loginHistory: [].toString(),
+    };
+
+    await sheet.addRow(newUser);
+    return new Response(
+      JSON.stringify({ message: "User created successfully" }),
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error("Error creating user:", error);
+    return new Response(JSON.stringify({ error: "Error creating user" }), {
+      status: 500,
+    });
   }
+}
